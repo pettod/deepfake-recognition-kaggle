6e74af6d03086c81a4483af6053ecbd7ead6fd12
peter <peter.todorov@live.com>
2020-02-29 16:13:27 +0200

    Clean 'df.py'

--- df.py
+++ df.py
@@ -8,13 +8,13 @@ from tensorflow.python.client import device_lib
 
 print(device_lib.list_local_devices())
 detector = MTCNN()
-files = os.listdir('.\\Deepfake\\test_videos\\')
+files = os.listdir("test_videos/")
 faces = []
 
 
 for file in files:
-    VIDEO_DIR = ".\\Deepfake\\test_videos\\video_%s" % (file)
-    FACE_DIR = ".\\Deepfake\\faces\\video_%s" % (file)
+    VIDEO_DIR = "test_videos/video_%s" % (file)
+    FACE_DIR = "faces/video_%s" % (file)
 
     cnt = 1
     os.mkdir(FACE_DIR)
@@ -27,7 +27,7 @@ for file in files:
         #    break
         #img = cv2.imread(file) 
         #plt.imshow(img)
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         box = detector.detect_faces(img)
         if(len(box)==0):
@@ -35,7 +35,7 @@ for file in files:
 
         box = box[0]["box"]
         img = img[box[1]:box[1]+box[3], box[0]:box[0]+box[2]]
-        cv2.imshow("frame", img)
-        cv2.imwrite(FACE_DIR + "\\%03d.png" % (cnt), img)
+        #cv2.imshow("frame", img)
+        cv2.imwrite(FACE_DIR + "/%03d.png" % (cnt), img)
         cnt+=1
 
