9608e4ed0bc30e39f829394611d44cb2ffcf035e
peter <peter.todorov@live.com>
2020-03-06 20:19:52 +0200

    Make resnet50 to run epochs

--- resnet_code/resnet50_train.py
+++ resnet_code/resnet50_train.py
@@ -1,65 +1,154 @@
 import math, json, os, sys
-
+import tensorflow as tf
 import keras
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.layers import Dense
+from keras.layers import Dense, Flatten, Input
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.preprocessing import image
-
+import numpy as np
 
 DATA_DIR = "../cropped_faces/resnet_data"
-TRAIN_DIR = os.path.join(DATA_DIR, "train")
-VALID_DIR = os.path.join(DATA_DIR, "valid")
+TRAIN_DIR = DATA_DIR + "/train"
+VALID_DIR = DATA_DIR + "/train"
 SIZE = (224, 224)
-BATCH_SIZE = 16
+BATCH_SIZE = 4
+
+
+def resnet_v2(input_shape, depth, num_classes=10):
+    """ResNet Version 2 Model builder [b]
+
+    Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
+    bottleneck layer
+    First shortcut connection per layer is 1 x 1 Conv2D.
+    Second and onwards shortcut connection is identity.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filter maps is
+    doubled. Within each stage, the layers have the same number filters and the
+    same filter map sizes.
+    Features maps sizes:
+    conv1  : 32x32,  16
+    stage 0: 32x32,  64
+    stage 1: 16x16, 128
+    stage 2:  8x8,  256
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 9 != 0:
+        raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
+    # Start model definition.
+    num_filters_in = 16
+    num_res_blocks = int((depth - 2) / 9)
+
+    inputs = Input(shape=input_shape)
+    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
+    x = resnet_layer(inputs=inputs,
+                     num_filters=num_filters_in,
+                     conv_first=True)
+
+    # Instantiate the stack of residual units
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:  # first layer and first stage
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:  # first layer but not first stage
+                    strides = 2    # downsample
+
+            # bottleneck residual unit
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters_in,
+                             kernel_size=1,
+                             strides=strides,
+                             activation=activation,
+                             batch_normalization=batch_normalization,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_in,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_out,
+                             kernel_size=1,
+                             conv_first=False)
+            if res_block == 0:
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters_out,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = keras.layers.add([x, y])
+
+        num_filters_in = num_filters_out
+
+    # Add classifier on top.
+    # v2 has BN-ReLU before Pooling
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
 
 
 def main():
-    num_train_samples = sum(
-        [len(files) for r, d, files in os.walk(TRAIN_DIR)])
-    num_valid_samples = sum(
-        [len(files) for r, d, files in os.walk(VALID_DIR)])
+    with tf.device("/device:GPU:0"):
+        num_train_samples = sum(
+            [len(files) for r, d, files in os.walk(TRAIN_DIR)])
+        num_valid_samples = sum(
+            [len(files) for r, d, files in os.walk(VALID_DIR)])
 
-    num_train_steps = math.floor(num_train_samples/BATCH_SIZE)
-    num_valid_steps = math.floor(num_valid_samples/BATCH_SIZE)
+        num_train_steps = math.floor(num_train_samples/BATCH_SIZE)
+        num_valid_steps = math.floor(num_valid_samples/BATCH_SIZE)
 
-    gen = keras.preprocessing.image.ImageDataGenerator()
-    val_gen = keras.preprocessing.image.ImageDataGenerator(
-        horizontal_flip=True, vertical_flip=True)
+        gen = keras.preprocessing.image.ImageDataGenerator()
+        val_gen = keras.preprocessing.image.ImageDataGenerator(
+            horizontal_flip=True, vertical_flip=True)
 
-    batches = gen.flow_from_directory(
-        TRAIN_DIR, target_size=SIZE, class_mode="binary", shuffle=True,
-        batch_size=BATCH_SIZE)
-    val_batches = val_gen.flow_from_directory(
-        VALID_DIR, target_size=SIZE, class_mode="binary", shuffle=True,
-        batch_size=BATCH_SIZE)
+        batches = gen.flow_from_directory(
+            TRAIN_DIR, target_size=SIZE, class_mode="binary", shuffle=True,
+            batch_size=BATCH_SIZE)
+        val_batches = val_gen.flow_from_directory(
+            VALID_DIR, target_size=SIZE, class_mode="binary", shuffle=True,
+            batch_size=BATCH_SIZE)
+        input_shape = batches.next()[0].shape
+        model = keras.applications.resnet50.ResNet50(weights=None, classes=2)
 
-    model = keras.applications.resnet50.ResNet50()
+        finetuned_model = resnet_v2(batches, 56, num_classes=2)
+        finetuned_model.compile(
+            optimizer=Adam(lr=0.0001), loss="binary_crossentropy",
+            metrics=["accuracy"])
 
-    classes = list(iter(batches.class_indices))
-    model.layers.pop()
-    for layer in model.layers:
-        layer.trainable = False
-    last = model.layers[-1].output
-    x = Dense(len(classes), activation="softmax")(last)
-    finetuned_model = Model(model.input, x)
-    finetuned_model.compile(
-        optimizer=Adam(lr=0.0001), loss="binary_crossentropy",
-        metrics=["accuracy"])
-    for c in batches.class_indices:
-        classes[batches.class_indices[c]] = c
-    finetuned_model.classes = classes
+        early_stopping = EarlyStopping(patience=10)
+        checkpointer = ModelCheckpoint(
+            "resnet50_best.h5", verbose=1, save_best_only=True)
 
-    early_stopping = EarlyStopping(patience=10)
-    checkpointer = ModelCheckpoint(
-        "resnet50_best.h5", verbose=1, save_best_only=True)
-
-    finetuned_model.fit_generator(
-        batches, steps_per_epoch=num_train_steps, epochs=1000,
-        callbacks=[early_stopping, checkpointer], validation_data=val_batches,
-        validation_steps=num_valid_steps)
-    finetuned_model.save("resnet50_final.h5")
+        finetuned_model.fit_generator(
+            batches, steps_per_epoch=num_train_steps, epochs=1000,
+            callbacks=[early_stopping, checkpointer],
+            validation_data=val_batches,
+            validation_steps=num_valid_steps)
+        finetuned_model.save("resnet50_final.h5")
 
 
 main()
