7f88cf4e71006015d96be01090927d1c8e0d8494
peter <peter.todorov@live.com>
2020-03-15 22:42:19 +0200

    Create function to get faces

--- face_detection_test_2.py
+++ face_detection_test_2.py
@@ -1,48 +1,58 @@
 import cv2
 
 
-DNN = "CAFFE"
-if DNN == "CAFFE":
-    modelFile = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
-    configFile = "deploy.prototxt"
-    net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
-else:
-    modelFile = "opencv_face_detector_uint8.pb"
-    configFile = "opencv_face_detector.pbtxt"
-    net = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
+MODEL_FILE = "../input/face-detection-config-files/res10_300x300_ssd_iter_140000_fp16.caffemodel"
+CONFIG_FILE = "../input/face-detection-config-files/deploy.prototxt"
+VIDEO_PATH = "../input/deepfake-detection-challenge/test_videos/acazlolrpz.mp4"
 
-cap = cv2.VideoCapture("../input/deepfake-detection-challenge/test_videos/jhczqfefgw.mp4")
-while(cap.isOpened()):
-    ret, img = cap.read()
 
-    blob = cv2.dnn.blobFromImage(img, 1.0, (300, 300), [104, 117, 123], False, False)
+def getFaces(path, image_size, net):
+    cap = cv2.VideoCapture(path)
+    faces = []
+    if (not cap.isOpened()):
+        print(path)
+        print("VIDEO НЕ ОТКРЫВАЕТСЯ СУКА БЛЯТЬ")
+        return []
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_height = frame.shape[0]
+        frame_width = frame.shape[1]
+        blob = cv2.dnn.blobFromImage(
+            frame, 1.0, (300, 300), [104, 117, 123], False, False)
+        net.setInput(blob)
+        detections = net.forward()
+        conf_threshold = 0.5
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > conf_threshold:
+                x1 = int(detections[0, 0, i, 3] * frame_width)
+                y1 = int(detections[0, 0, i, 4] * frame_height)
+                x2 = int(detections[0, 0, i, 5] * frame_width)
+                y2 = int(detections[0, 0, i, 6] * frame_height)
+                face_width = x2 - x1
+                face_height = y2 - y1
+                if face_height < face_width:
+                    coordinate_change = (face_width - face_height) // 2
+                    x1 += coordinate_change
+                    x2 -= coordinate_change
+                else:
+                    coordinate_change = (face_height - face_width) // 2
+                    y1 += coordinate_change
+                    y2 -= coordinate_change
+                face = cv2.resize(frame[y1:y2, x1:x2], image_size)
+                faces.append(face)
+                #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        #cv2.imshow("y", frame)
+        #cv2.waitKey(0)
+    return faces
 
-    frame_height = img.shape[0]
-    frame_width = img.shape[1]
 
-    net.setInput(blob)
-    detections = net.forward()
-    bboxes = []
-    conf_threshold = 0.5
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > conf_threshold:
-            x1 = int(detections[0, 0, i, 3] * frame_width)
-            y1 = int(detections[0, 0, i, 4] * frame_height)
-            x2 = int(detections[0, 0, i, 5] * frame_width)
-            y2 = int(detections[0, 0, i, 6] * frame_height)
-            face_width = x2 - x1
-            face_height = y2 - y1
-            if face_height < face_width:
-                coordinate_change = (face_width - face_height) // 2
-                x1 += coordinate_change
-                x2 -= coordinate_change
-            else:
-                coordinate_change = (face_height - face_width) // 2
-                y1 += coordinate_change
-                y2 -= coordinate_change
-            print(x1, y1, x2, y2)
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.imshow("y", img)
-            cv2.waitKey(0)
-
+if __name__ == "__main__":
+    net = cv2.dnn.readNetFromCaffe(CONFIG_FILE, MODEL_FILE)
+    faces_in_video = getFaces(VIDEO_PATH, (224, 224), net)
+    print("Number of detected faces in video:", len(faces_in_video))
+    for face in faces_in_video:
+        cv2.imshow("face", face)
+        cv2.waitKey(0)
