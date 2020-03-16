224fe667b4fe6f41f700172d3097bd0fda5e40aa
peter <peter.todorov@live.com>
2020-03-16 08:48:27 +0200

    Add comments to face_detection code

--- face_detection_test_2.py
+++ face_detection_test_2.py
@@ -3,36 +3,47 @@ import cv2
 
 MODEL_FILE = "../input/face-detection-config-files/res10_300x300_ssd_iter_140000_fp16.caffemodel"
 CONFIG_FILE = "../input/face-detection-config-files/deploy.prototxt"
-VIDEO_PATH = "../input/deepfake-detection-challenge/test_videos/acazlolrpz.mp4"
+VIDEO_PATH = "../input/deepfake-detection-challenge/test_videos/gahgyuwzbu.mp4"
+EVERY_ITH_FRAME = 10
+IMAGE_SIZE = (224, 224)
 
 
-def getFaces(path, image_size, net):
+def getFaces(
+        path, image_size, net, every_ith_frame=1, confidence_threshold=0.5):
     cap = cv2.VideoCapture(path)
     faces = []
     if (not cap.isOpened()):
-        print(path)
-        print("VIDEO НЕ ОТКРЫВАЕТСЯ СУКА БЛЯТЬ")
-        return []
+        print("Cannot open video:", path)
+        return faces
     while(cap.isOpened()):
+
+        # Read video frame
         ret, frame = cap.read()
         if not ret:
             break
         frame_height = frame.shape[0]
         frame_width = frame.shape[1]
+
+        # Detect faces from frame
         blob = cv2.dnn.blobFromImage(
             frame, 1.0, (300, 300), [104, 117, 123], False, False)
         net.setInput(blob)
         detections = net.forward()
-        conf_threshold = 0.5
+
+        # Iterate all detections
         for i in range(detections.shape[2]):
             confidence = detections[0, 0, i, 2]
-            if confidence > conf_threshold:
+
+            # Detected face
+            if confidence > confidence_threshold:
                 x1 = int(detections[0, 0, i, 3] * frame_width)
                 y1 = int(detections[0, 0, i, 4] * frame_height)
                 x2 = int(detections[0, 0, i, 5] * frame_width)
                 y2 = int(detections[0, 0, i, 6] * frame_height)
                 face_width = x2 - x1
                 face_height = y2 - y1
+
+                # Make detected area square
                 if face_height < face_width:
                     coordinate_change = (face_width - face_height) // 2
                     x1 += coordinate_change
@@ -43,15 +54,15 @@ def getFaces(path, image_size, net):
                     y2 -= coordinate_change
                 face = cv2.resize(frame[y1:y2, x1:x2], image_size)
                 faces.append(face)
-                #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        #cv2.imshow("y", frame)
-        #cv2.waitKey(0)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.imshow("y", frame)
+        cv2.waitKey(0)
     return faces
 
 
 if __name__ == "__main__":
     net = cv2.dnn.readNetFromCaffe(CONFIG_FILE, MODEL_FILE)
-    faces_in_video = getFaces(VIDEO_PATH, (224, 224), net)
+    faces_in_video = getFaces(VIDEO_PATH, IMAGE_SIZE, net, EVERY_ITH_FRAME)
     print("Number of detected faces in video:", len(faces_in_video))
     for face in faces_in_video:
         cv2.imshow("face", face)
